e96edeac331ef5489583649c411175c165425888
Robert Jan Schlimbach <robertjan.schlimbach@gmail.com>
2020-08-15 13:28:28 +0200

    added data marginal plot util

---
+++ utils/plot.py
@@ -0,0 +1,22 @@
+import nrrd
+import numpy as np
+from tqdm import tqdm
+
+
+def compute_scan_data_marginal(scan_paths, n_id=5000, ignore_min=-1024):
+    '''
+    Expects data to be integers
+    Amount of intensity values (indexes) counted is (-ignore_min + n_id)
+
+    returns an array with the per-pixel-intensity count.
+    '''
+    hist_array = np.zeros((n_id,), dtype=np.int64)
+    for i, scan_path in enumerate(tqdm(self.scans)):
+        scan = nrrd.read(scan_path)[0]
+        if scan.min() != ignore_min:
+            print(f"Ignoring scan {scan_path}, min is {scan.min()}")
+            continue
+
+        values, counts = np.unique(scan, return_counts=True)
+        hist_array[values-ignore_min] += counts
+    return hist_array
